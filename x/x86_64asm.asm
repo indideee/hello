2715bb1524eb93b9f32f44dbfc4ba2132adc066c
indie <93843215+indideee@users.noreply.github.com>
2024-03-19 21:05:20 +1300

    add x86 assembly

---
+++ x/x86_64asm.asm
@@ -0,0 +1,20 @@
+; Hello, world! in x86_64 assembly
+
+global _start
+
+section .text
+
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, msglen
+  syscall
+
+  mov rax, 60
+  mov rdi, 0
+  syscall
+
+section .rodata
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
